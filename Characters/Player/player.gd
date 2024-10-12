e917cd483f8843a7961bbccbf669f2f47db2029b
Koray289 <koray.baskoca@tedu.edu.tr>
2024-10-12 16:47:35 +0300

    Update player.gd

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -9,6 +9,8 @@ const FRICTION = 500 * asd
 func _ready() -> void:
 	velocity = Vector2.ZERO
 
+#selamun aleykum
+
 func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
